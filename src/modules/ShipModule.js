fa58a05f1a1b6f3e44da4854106ce1236cd48d88
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-09-02 13:17:21 +0400

    further configuration done

--- src/modules/ShipModule.js
+++ src/modules/ShipModule.js
@@ -1,5 +1,20 @@
-class Ship{
-  constructor(){
-  
+export class Ship{
+  constructor(length){
+    this.length = length;
+    this.hit = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+   this.hit++;
+    if (this.hit == this.length){
+      this.sunk = true;
+      return;
+    }
+  }
+
+  isSunk(){
+    return this.sunk;
   }
 }
+
