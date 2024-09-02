669cc5a1480b8988ac6f348c0bebc12667c6359c
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-09-02 13:55:21 +0400

    temporary length control added

--- src/modules/ShipModule.js
+++ src/modules/ShipModule.js
@@ -1,11 +1,11 @@
 export class Ship{
   constructor(length){
-    this.length = length;
+    this.length = length >= 2 && length <= 5 ? length : console.log("value has to be between 2 and 5");
     this.hit = 0;
     this.sunk = false;
   }
 
-  hit() {
+  hitShip() {
    this.hit++;
     if (this.hit == this.length){
       this.sunk = true;
