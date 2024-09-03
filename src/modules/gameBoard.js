59cf726531ba5cf7446cb5b87654b1691ff4c59e
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-09-03 23:32:35 +0400

    fixing hard-coded length value

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,25 +1,31 @@
 export class GameBoard{
   constructor(){
-    this.grid = Array.from({ length: 10 }, () => Array(10).fill(0));
+    this.length = 10;
+    this.grid = Array.from({ length: this.length }, () => Array(this.length).fill(0));
+    this.shipsContainer = [];
+    this.missedShots = [];
   }
-
 placeShip(startCoordinate, direction, ship) {
   if (direction === "Horizontal") {
-    if (startCoordinate[1] + ship.length > 10) {
-      console.log("cannot place ship out of grid ie row wise");
+    if (startCoordinate[1] + ship.length > this.length) {
+      console.log('not placed')
       return false;
     }
     for (let i = 0; i < ship.length; i++) {
       this.grid[startCoordinate[0]][startCoordinate[1] + i] = "Ship";
-    }
+      this.shipsContainer.push(startCoordinate);
+      
+    }console.log("Placed!");
     return true;
-  } else if (direction === "vertical") {
-    if (ship.length + startCoordinate[0] > 10) {
-      console.log("cannot place ship column wise i.e out of grid");
+  } 
+  else if (direction === "vertical") {
+    if (ship.length + startCoordinate[0] > this.length) {
+      console.log('not placed')
       return false;
     }
     for (let i = 0; i < ship.length; i++) {
       this.grid[startCoordinate[0] + i][startCoordinate[1]] = "Ship";
+      this.shipsContainer.push(startCoordinate);
     }
     return true;
   }
@@ -29,23 +35,26 @@ placeShip(startCoordinate, direction, ship) {
 receiveAttack(coordinate) {
   const [x, y] = coordinate;
 
-  if (x < 0 || x >= 10 || y < 0 || y >= 10) {
+  if (x < 0 || x >= this.length || y < 0 || y >= this.length) {
     return false;
   }
   if (this.grid[x][y] === "Hit" || this.grid[x][y] === "Miss") {
     return false;
   } else if (this.grid[x][y] === "Ship") {
     this.grid[x][y] = "Hit";
+    console.log("Hit");
     return "Hit";
   } else {
     this.grid[x][y] = "Miss";
+    this.missedShots.push(coordinate);
+    console.log("Hit");
     return "Miss";
   }
 }
 
 allShipsSank() {
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < this.length; j++) {
       if (this.grid[i][j] === "Ship") {
         return false;
       }
